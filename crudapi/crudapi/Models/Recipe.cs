95371837a82f5583f3ca007adc2dc8c131dddffc
unknown <binag0204@gmail.com>
2022-02-02 23:03:16 -0500

    first commit

---
+++ crudapi/crudapi/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudapi.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public byte[] imagePath { get; set; }
+
+        public string ingredients { get; set; }
+
+    }
+}
---
+++ crudapi/crudapi/Models/RecipesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using crudapi.Models;
+namespace crudapi.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+
+        public DbSet<Recipe> Recipes { get; set; }
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options):base(options)
+        {
+        } 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recipe>().ToTable("Recipe");
+        }
+    }
+}
